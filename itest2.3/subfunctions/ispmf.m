ea563f8e31149d1ab82e69a9eab2494f9bf8fa83
Satoshi Hirose <satoshi.hirose@gmail.com>
2021-03-09 13:03:57 +0900

    Delete ispmf.m

--- itest2.3/subfunctions/ispmf.m
+++
@@ -1,7 +0,0 @@
-function y = ispmf(x)
-% check if x satisfies requirement for probability mass function
-% 1) x is a vector with values between 0 and 1
-% 2) sum of all array is 1
-
-numerical_error_tolerance = 10^(-9); 
-y= (isvector(x) && all(arrayfun(@isprob,x)) && (abs(sum(x)-1)<numerical_error_tolerance));
